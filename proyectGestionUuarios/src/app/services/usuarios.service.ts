6cf89a1dfa001356a7bea232770cf4c22c1e042e
Camilo Lobo <camilo.loboguerreron@konradlorenz.edu.co>
2021-12-16 14:00:03 -0500

    ultimas modificaciones

--- proyectGestionUuarios/src/app/services/usuarios.service.ts
+++ proyectGestionUuarios/src/app/services/usuarios.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getUsuarios() {
+    let header = new HttpHeaders()
+      .set('Type-content', 'application/json')
+
+    return this.http.get("/usuarios/usuario?page=1", {
+      headers: header
+    });
+
+  }
 }
